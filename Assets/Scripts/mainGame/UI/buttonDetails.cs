4c84a9db62e0e0aaf9a3629400542fa394ad9587
Ryan Everett <Ryanrocks2008@hotmail.com>
2016-04-15 16:47:23 -0400

    Begun work on Xbox UI integration with the HUD

---
+++ Assets/Scripts/mainGame/UI/buttonDetails.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonDetails : MonoBehaviour {
+
+    [SerializeField]private int ButtonCount = 1;
+    [SerializeField]private string weaponName = "";
+    [SerializeField]private float refreshTime = 1.0f;
+    [SerializeField]private GameObject progressRing;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
